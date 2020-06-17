3e1954e012fb471494b586c2a3796252fd77a309
Nitikesh R. Shinde <61821954+nitikeshs58@users.noreply.github.com>
2020-06-17 11:06:07 +0530

    Update Program.cs

--- GmailLogin/GmailLogin/Program.cs
+++ GmailLogin/GmailLogin/Program.cs
@@ -7,18 +7,6 @@ namespace GmailLogin
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            /*
-             public static void LoginGmail(IWebDriver driver, string email, string password ) 
-             { 
-                var loginBox = driver.FindElement(By.Id("Email"));
-                loginBox.SendKeys(email); 
-                var nextBtn = driver.FindElement(By.Id("next"));
-                nextBtn.Click(); 
-                var pwBox = driver.FindElement(By.Id("Passwd"));
-                pwBox.SendKeys(password); 
-                var signinBtn = driver.FindElement(By.Id("signIn")); 
-                signinBtn.Click(); }
-             */
         }
     }
 }
